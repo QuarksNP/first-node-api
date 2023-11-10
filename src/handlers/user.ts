65275b43b8c78a336304a1d7ff5f59c54406e917
Alberto_gzb <93887130+QuarksNP@users.noreply.github.com>
2023-11-10 13:01:47 -0400

    test

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,21 +1,17 @@
 import prisma from "../utils/prisma-client";
 import { comparePassword, createJWT, hashPassword } from "../modules/auth";
 
-export const createUser = async (req, res, next) => {
-    try {
-        const user = await prisma.user.create({
-            data: {
-                username: req.body.username,
-                password: await hashPassword(req.body.password)
-            }
-        })
+export const createUser = async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            password: await hashPassword(req.body.password)
+        }
+    })
 
-        const token = createJWT(user)
-        res.json({ token })
-    } catch (e) {
-        e.type = 'input'
-        next(e)
-    }
+    const token = createJWT(user)
+    res.json({ token })
+
 }
 
 export const signin = async (req, res) => {
--- src/server.ts
+++ src/server.ts
@@ -19,16 +19,9 @@ app.post('/user', createUser)
 app.post('/signin', signin)
 
 app.use((err, req, res, next) => {
-
-    if(err.type === 'auth') {
-        res.status(401).json({ message: "unauthenticated"})
-    } else if(err.type === 'input') {
-        res.status(400).json({ message: "invalid input" })
-    } else {
-        res.status(500).json({ message: "Sorry bro, It's my fault!"})
-    }
-
-})
+    console.log(err)
+    res.json({message: `had an error: ${err.message}`})
+  })
 
 export default app
 
