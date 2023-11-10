fa2a07370f186f8c4a05ec114864b1749417fe18
Alberto_gzb <93887130+QuarksNP@users.noreply.github.com>
2023-11-10 14:21:08 -0400

    implementing cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import morgan from 'morgan'
 import router from './router'
+import cors from 'cors'
 import { protect } from './modules/auth';
 import { createUser, signin } from './handlers/user';
 
 const app = express();
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
