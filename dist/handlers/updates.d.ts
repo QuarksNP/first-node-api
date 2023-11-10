4aa2635105b3529ba1558f1c81b2300826b190c3
Alberto_gzb <93887130+QuarksNP@users.noreply.github.com>
2023-11-10 12:17:42 -0400

    deploy

--- dist/handlers/updates.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const getOneUpdate: (req: any, res: any) => Promise<void>;
-export declare const getUpdates: (req: any, res: any) => Promise<void>;
-export declare const createUpdate: (req: any, res: any) => Promise<any>;
-export declare const updatedUpdate: (req: any, res: any) => Promise<any>;
-export declare const deletedUpdate: (req: any, res: any) => Promise<any>;
